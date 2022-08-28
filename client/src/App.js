33236ef4a9b541be7a0e28281d6590627c7da420
Jewel Hasan <jewelhasan477@gmail.com>
2022-08-28 20:15:28 +0600

    Front End almost done for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
-  return (
-    <>
-      <h1>Welcome to ProShop</h1>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <main className={ 'py-3' }>
+                <Container>
+                    <Routes>
+                        <Route exact path={ '/' } element={ <HomeScreen /> } />
+                        <Route path={ '/product/:id' } element={ <ProductScreen /> } />
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
